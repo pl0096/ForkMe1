bdc0a1790274e797d3da27df0c8caf979eea73ba
perlielui <perliel2026@student.cis.edu.hk>
2023-09-06 16:24:39 +0800

    Added main method and print statement.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("I am so rpoud of myself!");
+        System.out.println("I am so proud of myself!");
     }
 }
